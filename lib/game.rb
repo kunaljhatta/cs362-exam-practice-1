20356854ec0939a687eaa47dda802729a855fdd3
KunalJhatta <Kunaljhatta22@gmail.com>
2022-03-10 13:44:13 -0800

    passed the started test

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,15 @@
 class Game 
+    attr_accessor :started
     
+    def initialize
+        @is_started = false
+    end
+
     def start 
     end 
+
+    def started?
+        started
+    end
     
 end
